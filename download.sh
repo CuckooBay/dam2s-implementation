4278134bc534bc8486a8b3704ec5959f6508d2a7
Jared <gander.lacod@gmail.com>
2023-03-23 08:12:18 +0800

    fixed a bug in download script

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 url="http://rgbd-dataset.cs.washington.edu/dataset/rgbd-scenes-v2/rgbd-scenes-v2_imgs.zip"
 filename="rgbd-scenes-v2_imgs"
 wget ${url}
-rm "rgbd-scenes-v2_imgs.zip"
 unzip "rgbd-scenes-v2_imgs.zip"
+rm "rgbd-scenes-v2_imgs.zip"
 mv ${filename} "./data"
