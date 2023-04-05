57ab0bf041ef39129aac5c908d214c533a2facb1
Jared <gander.lacod@gmail.com>
2023-04-05 15:38:05 +0800

    Updated download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,10 @@
-url="http://rgbd-dataset.cs.washington.edu/dataset/rgbd-scenes-v2/rgbd-scenes-v2_imgs.zip"
-filename="rgbd-scenes-v2_imgs"
-wget ${url}
-unzip "rgbd-scenes-v2_imgs.zip"
-rm "rgbd-scenes-v2_imgs.zip"
-mv ${filename} "./data"
+obj="tomato soda_can mushroom lightbulb coffee_mug"
+for i in ${obj}
+do
+    url="http://rgbd-dataset.cs.washington.edu/dataset/rgbd-dataset_full/${i}_1.tar"
+    filename="rgbd-scenes-v2_${i}"
+    wget ${url}
+    unzip "${filename}.zip"
+    rm "${filename}.zip"
+    mv ${filename} "./data"
+done
